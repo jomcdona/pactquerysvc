623c7afb6cd3cea220f034134977e8a7d5a85527
jomcdona <jomcdona@redhat.com>
2020-09-09 14:07:13 -0500

    new project

---
+++ src/main/java/com/redhat/pactquerysvc/PactquerysvcApplication.java
@@ -0,0 +1,13 @@
+package com.redhat.pactquerysvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PactquerysvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PactquerysvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/redhat/pactquerysvc/PactquerysvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redhat.pactquerysvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PactquerysvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
