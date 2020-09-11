eb9292d1d591be3c3944cf3f649b524504f73fbf
jomcdona <jomcdona@redhat.com>
2020-09-11 18:23:54 +0000

    controller, service, prop changes

--- src/main/java/com/redhat/pactquerysvc/controller/PactQueryController.java
+++ src/main/java/com/redhat/pactquerysvc/controller/PactQueryController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import com.redhat.pactquerysvc.model.ContactDM;
 import com.redhat.pactquerysvc.service.PactQueryService;
 
-//@RestController
+@RestController
 public class PactQueryController {
 
     @Autowired
--- src/main/java/com/redhat/pactquerysvc/service/PactQueryService.java
+++ src/main/java/com/redhat/pactquerysvc/service/PactQueryService.java
@@ -1,12 +1,14 @@
 package com.redhat.pactquerysvc.service;
 
 import java.util.List;
+import org.springframework.stereotype.Service;
 
 import com.redhat.pactquerysvc.model.ContactDM;
 import com.redhat.pactquerysvc.repository.ContactRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+@Service
 public class PactQueryService {
 
     @Autowired
